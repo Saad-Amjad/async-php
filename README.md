37a076321c5dae589bbd182c1c0e3e9a6f9fb6f3
Saad-Amjad <saad.amjad@sekai-lab-bd.com>
2021-05-26 23:27:52 +0600

    added amp php example

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Asynchronous PHP
  
 Series of examples to demonstrate asynchronous PHP for the talk State of Async PHP.
+
+## Running the examples
+
+After running `composer install`, in the terminal just type in:
+`php examples/{your-prefered-example-file.php}`
+This will run the given example.
---
+++ examples/amp-server.php
@@ -0,0 +1,48 @@
+<?php
+
+require './vendor/autoload.php';
+
+use Amp\ByteStream\ResourceOutputStream;
+use Amp\Http\Server\HttpServer;
+use Amp\Http\Server\RequestHandler\CallableRequestHandler;
+use Amp\Http\Server\Response;
+use Amp\Http\Status;
+use Amp\Log\ConsoleFormatter;
+use Amp\Log\StreamHandler;
+use Amp\Socket;
+use Monolog\Logger;
+
+// Run this script, then visit http://localhost:1337/ or https://localhost:1338/ in your browser.
+
+Amp\Loop::run(static function () {
+    $cert = new Socket\Certificate(__DIR__ . '/../test/server.pem');
+
+    $context = (new Socket\BindContext)
+        ->withTlsContext((new Socket\ServerTlsContext)->withDefaultCertificate($cert));
+
+    $servers = [
+        Socket\Server::listen("0.0.0.0:1337"),
+        Socket\Server::listen("[::]:1337"),
+        Socket\Server::listen("0.0.0.0:1338", $context),
+        Socket\Server::listen("[::]:1338", $context),
+    ];
+
+    $logHandler = new StreamHandler(new ResourceOutputStream(STDOUT));
+    $logHandler->setFormatter(new ConsoleFormatter);
+    $logger = new Logger('server');
+    $logger->pushHandler($logHandler);
+
+    $server = new HttpServer($servers, new CallableRequestHandler(static function () {
+        return new Response(Status::OK, [
+            "content-type" => "text/plain; charset=utf-8"
+        ], "Hello Amp World!");
+    }), $logger);
+
+    yield $server->start();
+
+    // Stop the server when SIGINT is received (this is technically optional, but it is best to call Server::stop()).
+    Amp\Loop::onSignal(\SIGINT, static function (string $watcherId) use ($server) {
+        Amp\Loop::cancel($watcherId);
+        yield $server->stop();
+    });
+});
